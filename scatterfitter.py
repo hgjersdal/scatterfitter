e8c02eeb2eddb07386f72de20e06db4ea837562d
Håvard Gjersdal <havard.gjersdal@fys.uio.no>
2023-09-21 15:49:58 +0200

    Updated plotting

--- scatterfitter.py
+++ scatterfitter.py
@@ -45,7 +45,7 @@ def make_fit(params):
     fit = np.zeros(data.shape)
     for x in range(500):
         for y in range(500):
-            rsq = (x-249)**2 + (y-249)**2
+            rsq = (x-249.5)**2 + (y-249.5)**2
             fit[x, y] += multi_gauss(rsq, params)
     return fit
 
@@ -77,36 +77,74 @@ def make_scatter_plot():
     return radii, vals
 
 
-# [1.12345079e+01 9.58922768e+08],15693218.86835867
-# [1.08207483e+01 9.20767764e+08 2.56762130e+01 5.65186091e+07],6510213.87314924
-# [1.07181042e+01 9.05804308e+08 2.26886486e+01 6.88118813e+07  8.24731128e+01 5.61262252e+06],4837986.627212546
-# [1.05377247e+01 8.23557276e+07 1.86380095e+01 5.33742117e+06 4.38545477e+01 2.75531134e+05 1.82795339e+02 1.67310713e+04],4081402.2629849515
-# [1.05357713e+01 8.23537029e+07 1.85982212e+01 5.33818541e+06 4.05107626e+01 2.75242403e+05 8.39473584e+01 2.81355032e+04 2.65996038e+02 1.04448512e+04],4035015.2437909436
-# [1.05362562e+01, 8.23554263e+07, 1.85514032e+01, 5.33488480e+06, 4.02583140e+01, 2.89389124e+05, 8.66094458e+01, 2.62153542e+04, 2.68955978e+02, 1.03075160e+04],4031452.2802090473
-params = [ 1.05261568e+01, 8.72872199e+08, 1.92035164e+01, 9.56196076e+07, 4.77387466e+01, 9.58393595e+06, 2.04397747e+02, 3.45646467e+06, 3.44612096e+03, 8.48815010e+04]
-
-# fit = make_fit(params)
-# plt.matshow(fit - data)
-# plt.show()
-
 
 # Make a scatterplot of the data
 radii, vals = make_scatter_plot()
-plt.scatter(radii, vals)
+plt.scatter(radii, vals, s=4)
 print(radii)
 
 
 # Plot the Gaussian sum desctibed by params
-def mga(r):
-    rr = r**2
-    return multi_gauss(rr, params)
+def mult_gauss_plot(params, xs, lbl):
+    def mga(x):
+        return multi_gauss(x**2, params)
+    ys = np.array([mga(r) for r in xs])
+    ys[ys < 1] = 1
+    plt.plot(xs, ys, label=lbl)
+
 
 
 xs = np.linspace(-250, 250, 500)
-ys = np.array([mga(r) for r in xs])
-plt.plot(xs, ys, color='red')
+
+params6 = [1.03538463e+01, 8.03250571e+08, 1.50521177e+01, 1.38563360e+08, 2.48976758e+01, 2.92682121e+07, 2.83451220e+02, 2.79124680e+06, 4.50377575e+01, 7.44242623e+06, 9.18093612e+01, 2.05398347e+06]
+params5 = [1.04182992e+01, 8.30840063e+08, 1.63239725e+01, 1.24554259e+08,  3.08056288e+01, 2.05431747e+07, 2.34480576e+02, 2.80489374e+06,  6.61299463e+01, 4.24356058e+06]
+params4 = [1.05377247e+01, 8.67841985e+08, 1.86380093e+01, 9.94789108e+07, 4.38545434e+01, 1.20832954e+07, 1.82795365e+02, 3.05836237e+06]
+params3 = [1.06933752e+01, 9.01105354e+08, 2.20956316e+01, 7.34603862e+07, 7.66777886e+01, 6.07651451e+06]
+params2 = [1.08207672e+01, 9.20774754e+08, 2.56767779e+01, 5.65140248e+07]
+params1= [1.12345079e+01, 9.58922768e+08]
+
+mult_gauss_plot(params6, xs, "6")
+mult_gauss_plot(params5, xs, "5")
+mult_gauss_plot(params4, xs, "4")
+mult_gauss_plot(params3, xs, "3")
+mult_gauss_plot(params2, xs, "2")
+mult_gauss_plot(params1, xs, "1")
+
 plt.yscale("log")
+plt.legend()
 plt.show()
+
+
+plt.plot(xs, data[249, :])
+mult_gauss_plot(params6, xs, "6")
+mult_gauss_plot(params5, xs, "5")
+mult_gauss_plot(params4, xs, "4")
+mult_gauss_plot(params3, xs, "3")
+mult_gauss_plot(params2, xs, "2")
+mult_gauss_plot(params1, xs, "1")
+plt.yscale("log")
+plt.legend()
+plt.show()
+
+
+fit = make_fit(params1)
+pulls = (fit - data)/np.sqrt(var)
+plt.matshow(pulls)
+plt.show()
+
+fit = make_fit(params3)
+pulls = (fit - data)/np.sqrt(var)
+plt.matshow(pulls)
+plt.show()
+
+fit = make_fit(params6)
+pulls = (fit - data)/np.sqrt(var)
+plt.matshow(pulls)
+plt.show()
+
+
+
+params = params6
 
 while True:
  result = minimize(chi_squared, params, method='Nelder-Mead')

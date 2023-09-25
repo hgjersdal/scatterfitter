d64449a07ec1487eabbb40b388791db9e887f676
Håvard Gjersdal <havard.gjersdal@fys.uio.no>
2023-09-25 16:44:05 +0200

    Cleaned up the code

--- scatterfitter.py
+++ scatterfitter.py
@@ -14,14 +14,12 @@ def read_data():
         data_array = np.array(data, dtype=float)
         return data_array[250:750, 250:750]
 
+
 data = read_data()
-print(data)
-plt.matshow(data)
-plt.show()
 
 # The Poisson variance is the expexted mean. The expected mean is the number of hits in the bin, or 1.
 var = data
-var[var<1] = 1
+var[var < 1] = 1
 
 
 def gaussian(rsqr, sigma, A):
@@ -33,30 +31,18 @@ def gaussian(rsqr, sigma, A):
 
 
 def multi_gauss(rsqr, params):
-    """ Several Gausses on top of each other """
+    """ Several Gausses on top of each other at poit rsqr"""
     val = 0
     for i in range(len(params)//2):
         val += gaussian(rsqr, params[i*2], params[i*2+1])
     return val
 
-def gauss_v(rsqrs, sigma, A):
-    return (A / (sigma**2 * 2 * np.pi)) * np.exp(- 0.5 * rsqrs/(sigma*sigma))
 
 def multi_gauss_v(rsqrs, params):
-    """ Several Gausses on top of each other """
+    """ An image of several Gausses on top of each other """
     fit = np.zeros(data.shape)
     for i in range(len(params)//2):
-        fit = fit + gauss_v(rsqrs, params[i*2], params[i*2+1])
-    return fit
-
-
-def make_fit2(params):
-    """ Make a matric from a set of gaussian parameters """
-    fit = np.zeros(data.shape)
-    for x in range(500):
-        for y in range(500):
-            rsq = (x-249.5)**2 + (y-249.5)**2
-            fit[x, y] += multi_gauss(rsq, params)
+        fit = fit + gaussian(rsqrs, params[i*2], params[i*2+1])
     return fit
 
 
@@ -65,8 +51,6 @@ def make_fit(params):
     x, y = data.shape
     X, Y = np.ix_(np.arange(x), np.arange(y))
     rsqs = (X-249.5)**2 + (Y-249.5)**2
-    # vfunc = np.vectorize(lambda x: multi_gauss(x, params))
-    # fit = vfunc(rsqs)
     fit = multi_gauss_v(rsqs, params)
     return fit
 
@@ -94,14 +78,11 @@ def make_scatter_plot():
             radii[index] = radius
             vals[index] = data[x, y]
             index += 1
-            # print(str(radius) + ', ' + str(data[x, y]))
     return radii, vals
 
 
-# Make a scatterplot of the data
 radii, vals = make_scatter_plot()
 plt.scatter(radii, vals, s=4)
-print(radii)
 
 
 # Plot the Gaussian sum desctibed by params
@@ -113,23 +94,20 @@ def mult_gauss_plot(params, xs, lbl):
     plt.plot(xs, ys, label=lbl)
 
 
-
 xs = np.linspace(-250, 250, 500)
 
-params7 = [1.03820983e+01, 8.13167922e+08, 1.51130599e+01, 1.36396559e+08,
-           2.49682972e+01, 2.90299120e+07, 2.83680246e+02, 2.79125608e+06,
-           4.51206710e+01, 7.40459841e+06, 9.19499361e+01, 2.04799451e+06,
-           2.5e+02, 1.0e+04]
+params7 = [7.31857717e+00, 3.97705056e+08, 1.01497237e+01, 7.01126668e+07,
+           1.52928704e+01, 1.84648017e+07, 2.46103530e+01, 5.32810172e+06,
+           2.26335109e+02, 1.41095479e+06, 4.11094881e+01, 1.80758619e+06,
+           7.98294254e+01, 7.19943862e+05]
 
+params6 = [7.34125207e+00, 4.06583961e+08, 1.06865471e+01, 6.81982812e+07,
+           1.76552528e+01, 1.45149557e+07, 3.19051335e+01, 3.70229876e+06,
+           2.00592229e+02, 1.39562804e+06, 6.50184237e+01, 1.02399722e+06]
 
-params6 = [1.03820983e+01, 8.13167922e+08, 1.51130599e+01, 1.36396559e+08,
-           2.49682972e+01, 2.90299120e+07, 2.83680246e+02, 2.79125608e+06,
-           4.51206710e+01, 7.40459841e+06, 9.19499361e+01, 2.04799451e+06]  # 266179.14829355833
-
-params5 = [7.46811257e+00, 4.37921415e+08, 1.31558293e+01, 4.92052362e+07,
-           3.03388105e+01, 6.18073779e+06, 1.09783511e+02, 1.17969967e+06,
-           1.88718419e+02, 4.74452193e+05]
-
+params5 = [7.38583638e+00, 4.20137154e+08, 1.15930698e+01, 6.14089183e+07,
+           2.18666216e+01, 1.01749422e+07, 4.69140562e+01, 2.10508454e+06,
+           1.66107609e+02, 1.40204767e+06]
 
 params4 = [7.46823288e+00, 4.38245959e+08, 1.32290890e+01, 4.91958670e+07,
            3.11006356e+01, 5.99772212e+06, 1.29497860e+02, 1.52766408e+06]
@@ -147,13 +125,12 @@ converged_chi2s = [11892952.087187286,  # 1
                    1093369.2133564858,  # 3
                    346589.4656824806,  # 4
                    272925.0193639359,  # 5
-                   266179.14829355833  # 6
+                   266179.14829355833,  # 6
+                   265711.9130558083  # 7
                    ]
 
 
-
-
-
+mult_gauss_plot(params7, xs, "7")
 mult_gauss_plot(params6, xs, "6")
 mult_gauss_plot(params5, xs, "5")
 mult_gauss_plot(params4, xs, "4")
@@ -167,6 +144,7 @@ plt.show()
 
 
 plt.plot(xs, data[249, :])
+mult_gauss_plot(params7, xs, "7")
 mult_gauss_plot(params6, xs, "6")
 mult_gauss_plot(params5, xs, "5")
 mult_gauss_plot(params4, xs, "4")
@@ -178,6 +156,7 @@ plt.legend()
 plt.show()
 
 
+# Plot pull-values.
 fit = make_fit(params1)
 pulls = (fit - data)/np.sqrt(var)
 plt.matshow(pulls)
@@ -208,21 +187,20 @@ pulls = (fit - data)/np.sqrt(var)
 plt.matshow(pulls)
 plt.show()
 
+fit = make_fit(params7)
+pulls = (fit - data)/np.sqrt(var)
+plt.matshow(pulls)
+plt.show()
 
+params = params7
 
-params = params5
-
-
-print(data.sum())
 
 while True:
- result = minimize(chi_squared, params, method='Nelder-Mead')
- # result = minimize(chi_squared, params)
- # result = minimize(chi_squared, params, method='SLSQP')
- if result.success:
-     fitted_params = result.x
-     print(fitted_params)
-     break
- else:
-     print(result.message)
-     params = result.x
+    result = minimize(chi_squared, params, method='Nelder-Mead')
+    if result.success:
+        fitted_params = result.x
+        print(fitted_params)
+        break
+    else:
+        print(result.message)
+        params = result.x
